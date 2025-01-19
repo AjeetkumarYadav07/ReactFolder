e704afc4d56acd1509146a1da8563caeadd3b87a
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2025-01-19 22:45:01 +0530

    add another project code

---
+++ ReactBasic02/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import About from './screens/About' ;
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       {/* <h1> Hy Ajeet Kumar</h1> */}
+       <About/>
+    </>
+  )
+}
+
+export default App;
---
+++ ReactBasic02/src/screens/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <>
+    <h3>this About screen !</h3>
+   </>
+  )
+}
+
+export default About;
